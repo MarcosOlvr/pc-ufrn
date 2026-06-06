5e08c95c1744c7037959107ca42921a8aa90b4e2
Marcos Oliveira <marquinhos.dev@gmail.com>
2026-06-06 15:22:21 -0300

    resolvendo u-3/l-3/q3/4

---
+++ unidade-3/lista-2/questao3.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+
+    if x % 2 == 0:
+        x += 1
+
+    soma = 0
+
+    for j in range(y):
+        soma += x + (j * 2)
+
+    print(soma)
